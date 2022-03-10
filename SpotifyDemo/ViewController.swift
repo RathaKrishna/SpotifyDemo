0fae0d0d50ae27acf3e3683b0e6e75c712673e73
Rathakrishnan <born4winkrishna@gmail.com>
2022-03-10 09:01:44 +0800

    Initial Commit

---
+++ SpotifyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyDemo
+//
+//  Created by Rathakrishnan Ramasamy on 2022/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpotifyDemoUITests/SpotifyDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpotifyDemoUITestsLaunchTests.swift
+//  SpotifyDemoUITests
+//
+//  Created by Rathakrishnan Ramasamy on 2022/3/10.
+//
+
+import XCTest
+
+class SpotifyDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
